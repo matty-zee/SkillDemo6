b59eff25f7e2edcdc0316c89f07824199821fd05
matty-zee <97648068+matty-zee@users.noreply.github.com>
2022-02-03 14:40:00 -0800

    Creating a failing test for triple

--- SkillDemo6.java
+++ SkillDemo6.java
@@ -1,1 +1,6 @@
-public class SkillDemo6{}
+public class SkillDemo6{
+
+    public static int triple(int x){
+        return 3*x;
+    }
+}
---
+++ SkillDemo6Test.java
@@ -0,0 +1,9 @@
+import static org.junit.Assert.*;
+import org.junit.*;
+public class SkillDemo6Test {
+    
+    @Test
+    public void testTriple(){
+        assertEquals(2, SkillDemo6.triple(2));
+    }
+}
