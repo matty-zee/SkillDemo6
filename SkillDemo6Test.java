1a23b9aa6748aaa57213a390ee0cb601b9e058e7
matty-zee <97648068+matty-zee@users.noreply.github.com>
2022-02-03 14:42:10 -0800

    Update SkillDemo6Test.java

--- SkillDemo6Test.java
+++ SkillDemo6Test.java
@@ -1,9 +1,8 @@
 import static org.junit.Assert.*;
 import org.junit.*;
 public class SkillDemo6Test {
-    
     @Test
     public void testTriple(){
-        assertEquals(2, SkillDemo6.triple(2));
+        assertEquals(6, SkillDemo6.triple(2));
     }
 }
